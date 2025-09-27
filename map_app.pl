4f17d3936acdac493c2279b0341b7139f0827534
Marco Silva <arthurpbs@gmail.com>
2025-09-27 09:18:13 -0300

    Add detail endpoint
    Add the api endpoint to query details based on record id.

--- map_app.pl
+++ map_app.pl
@@ -25,4 +25,22 @@ get '/api/geojson' => sub ($c) {
   $c->render( text => $c->find_municipio($city_name) );
 };
 
+get '/api/details' => sub ($c) {
+  my $fid = $c->param('fid');
+  return unless $fid;
+  my @params = ( 
+    { fid => $fid }, 
+    { 
+      columns => [ 
+        qw( 
+          fid  cd_mun  nm_mun  cd_rgi  nm_rgi  cd_rgint  nm_rgint  cd_uf  nm_uf  
+          sigla_uf  cd_regia  nm_regia  sigla_rg  cd_concu  nm_concu  area_km2 
+        )
+      ],
+      result_class => 'DBIx::Class::ResultClass::HashRefInflator',
+    }
+  );
+  $c->render( json => $c->municipios->search_rs( @params )->first );
+};
+
 app->start;
