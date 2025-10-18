ed9904ce4fe74e27a98862d710758d6907d52d02
Marco Silva <arthurpbs@gmail.com>
2025-10-18 09:32:33 -0300

    refactor(backend): separate pkg for osm service
    - now MyApp::OSM::Query handles OSM overpass web service
    - create functional tests for it

---
+++ lib/MyApp/OSM/Service.pm
@@ -0,0 +1,154 @@
+package MyApp::OSM::Service;
+use Mojo::Base 'Mojo::EventEmitter', -signatures, -async_await;
+use Mojo::JSON qw(decode_json encode_json);
+use List::Util qw(first);
+use Mojo::URL;
+use Mojo::Log;
+use Mojo::File qw(path);
+use Mojo::UserAgent;
+use Time::HiRes qw(gettimeofday tv_interval);
+use DDP;
+
+has log           => sub { Mojo::Log->new };
+has polygon       => sub { die 'Need a polygon' };
+has timeout       => sub { 3*60 };
+has query         => sub ($self) { $self->_build_query };
+has _overpass_url => sub {
+  state $url = Mojo::URL->new('https://overpass-api.de/api/interpreter');
+};
+has _ua           => sub ($self) { Mojo::UserAgent->new->connect_timeout($self->timeout) };
+has _osm_raw      => sub { die 'require run_query() first' };
+has _osm_geojson  => sub { die 'require run_query() first' };
+
+# how many decimal points in coordinates
+has _precision    => sub { 6 };
+
+
+# for OFFLINE testing only
+has _testing_data => sub {
+  require Mojo::JSON;
+  my $raw = Mojo::JSON::decode_json(path('t', 'raw_osm.json')->slurp);
+  return Mojo::Promise->resolve($raw);
+};
+
+has _testing => sub { 0 };
+
+sub _poly_string($self) {
+  my @coords;
+  my $digits = $self->_precision;
+  my $format = "%.${digits}f %.${digits}f";
+  for my $poly ($self->polygon->{coordinates}->@*) {
+    my $ring = $poly->[0];
+    push @coords, map { sprintf($format, $_->[1], $_->[0]) } @$ring;
+  }
+  return join(' ', @coords);
+}
+
+sub _build_query($self) {
+  my $poly_str = $self->_poly_string;
+  my $tout = $self->timeout;
+  return <<~"QUERY";
+  [out:json][timeout:$tout];
+  (
+    way["landuse"](poly:"$poly_str");
+    way["natural"](poly:"$poly_str");
+    way["leisure"](poly:"$poly_str");
+    way["man_made"](poly:"$poly_str");
+  );
+  out body;
+  >;
+  out skel qt;
+  QUERY
+}
+
+async sub run_query_p($self) {
+  $self->log->info(sprintf 'Requesting OSM service...');
+  my $q = $self->_build_query;
+  $self->emit( query => $q );
+  my $data = await $self->_testing ? $self->_testing_data : $self->_get_from_osm($q);
+  $self->emit(osm_data => $data);
+  return $self->_osm_raw($data);
+}
+
+async sub _get_from_osm($self, $q = $self->_build_query) {
+  $self->log->info("Getting data from OSM service");
+  $self->emit( progress => { total => 0, processed => 0, phase => 'osm' } );
+  my $t0 = [ gettimeofday ];
+  my $tx = await $self->_ua->post_p( $self->_overpass_url => form => { data => $q } );
+  my $res = $tx->res;
+
+  if ($res->is_success) {
+    my $osm_data = {
+      query => $q,
+      data  => $res->body,
+      elapsed => tv_interval($t0),
+    };
+    $self->emit( query_data => $osm_data );
+  } else {
+    $self->log->error( sprintf "Failed query: %s, Error %s", $q, $res->message );
+  }
+  return decode_json($res->body);
+}
+
+sub run_query($self) { 
+  $self->run_query_p->wait; 
+  $self->_process_raw_data;
+}
+
+sub _process_raw_data($self, $osm_data = $self->_osm_raw) {
+  $self->log->info('Processing OSM raw data into GeoJSON format');
+  my @nodes = grep { $_->{type} eq 'node' } $osm_data->{elements}->@*;
+  my @ways;
+  my $total = scalar( $osm_data->{elements}->@* ) - scalar( @nodes );
+  my $processed = 0;
+
+  $self->emit(progress => {total => ($total-1), processed => $processed, phase => 'geojson'});
+
+  foreach my $el ($osm_data->{elements}->@*) {
+    next unless $el->{type} eq 'way';
+    my $props = { properties => { $el->{tags}->%* , id => $el->{id} } };
+    my $coords = [];
+
+    # find the nodes of polygon
+    foreach my $node ($el->{nodes}->@*) {
+      my $n = first { $_->{id} eq $node } @nodes;
+      push @$coords, [$n->{lon}, $n->{lat}];
+    }
+    my $type;
+    if (
+      $coords->[0][0] == $coords->[-1][0]
+      &&
+      $coords->[0][1] == $coords->[-1][1]
+    ) {
+      $type = 'Polygon';
+    } else {
+      $type = 'LineString';
+    }
+    my $feat = {
+      type => 'Feature',
+      geometry => { 
+        type => $type,
+        coordinates => $type eq 'LineString' ? $coords : [ $coords ],
+      },
+      $props->%*
+    };
+    push @ways, $feat;
+    $self->emit( feature => $feat );
+    $self->emit( progress => { total => $total, processed => ++$processed, phase => 'geojson' });
+  }
+  # make geojson
+  my $json = { type => 'FeatureCollection', features => [@ways] };
+  $self->_osm_geojson($json);
+  return $json
+}
+
+1;
+
+__END__
+
+
+=head1 DESCRIPTION
+
+Fetches geographic features (landuse, natural areas, leisure facilities, and man-made structures)
+within municipal boundaries and converts them to GeoJSON format querin OpenStreetMap
+by overpass-api.
---
+++ t/osm_service.t
@@ -0,0 +1,67 @@
+use Mojo::Base -strict, -signatures;
+use Test2::V0;
+use Test2::Tools::Compare qw(T F D DF E DNE FDNE U L);
+use lib qw(./lib);
+use MyApp::OSM::Service;
+use Mojo::JSON qw(decode_json encode_json);
+use DDP;
+
+my $json_str =<<'EOS';
+{
+  "type": "Polygon",
+  "coordinates": [
+    [
+      [
+        [-45.0811, -23.4439],
+        [-45.0611, -23.4439],
+        [-45.0611, -23.4239],
+        [-45.0811, -23.4239],
+        [-45.0811, -23.4439]
+      ]
+    ]
+  ]
+}
+EOS
+
+my $osm = MyApp::OSM::Service->new( polygon => decode_json($json_str) );
+
+subtest create_obj => sub {
+  ok $osm->isa('MyApp::OSM::Service'), 'ok type';
+};
+
+subtest run_query => sub {
+  my $poly_str = q(poly:"-23.4439 -45.0811 -23.4439 -45.0611 -23.4239 -45.0611 -23.4239 -45.0811 -23.4439 -45.0811");
+  $osm->_precision(4);
+  $osm->on(
+    query => sub ($evt, $query) { 
+      like $query, qr/\Q$poly_str\E/, 'expected poly string conversion';
+    }
+  );
+  $osm->on(
+    feature => sub ($evt, $feat) {
+      is (
+        $feat,
+        hash {
+          field geometry    => L();
+          field properties  => L();
+          field type        => L();
+        },
+        'required top‑level members are present'
+      );
+    }
+  );
+
+  # make it offline
+  $osm->_testing(1);
+  my $geojson = $osm->run_query;
+  is (
+    $geojson,
+    hash {
+      field type      => L();
+      field features  => L();
+    },
+    'correct geojson hash'
+  );
+};
+
+done_testing;
