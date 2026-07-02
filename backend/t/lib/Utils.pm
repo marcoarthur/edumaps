e8f70c70bb330807a69043cbdb6f658b69cdd838
Marco Silva <arthurpbs@gmail.com>
2026-07-02 19:01:25 -0300

    refactor(test): improving test utils
    Complement the utilities in test

--- backend/t/lib/Utils.pm
+++ backend/t/lib/Utils.pm
@@ -4,21 +4,27 @@ use Mojo::Collection qw(c);
 use EduMaps::Schema;
 use Exporter 'import';
 
-our @EXPORT_OK = qw(filter_resultsets c random_schools_ids random_schools_with_grades);
+our @EXPORT_OK = qw(filter_resultsets c random_schools_ids random_schools_with_grades random_city_id);
 our $sch = EduMaps::Schema->go;
 
 sub filter_resultsets($filter_cb) {
   c($sch->sources)->map( sub { $sch->resultset($_ ) } )->grep( $filter_cb );
 }
 
-sub random_schools_ids {
+sub random_schools_ids ($size = 10){
   $sch->resultset('CensoEscolas')
-  ->columns(['co_entidade'])->random_sample(10)->get_all;
+  ->columns(['co_entidade'])->random_sample($size)->get_all;
 }
 
-sub random_schools_with_grades {
+sub random_schools_with_grades ($size = 10){
   $sch->resultset('IdebNotasEscolas')->columns(['id_escola'])
-  ->random_sample(10)->get_all;
+  ->random_sample($size)->get_all;
+}
+
+sub random_city_id ($size = 10) {
+  $sch->resultset('MunicipiosSp')->random_sample($size)
+  ->columns(['codigo_ibge', 'nome_municipio'])
+  ->get_all;
 }
 
 1;
